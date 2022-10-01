6201ca54391bffc1e3eaaf5b2ff14726f612ecbf
madadi.majid@gmail.com <madadi.majid@gmail.com>
2022-10-01 12:08:13 +0200

    Add project files.

---
+++ AutomationTask2.Tests/PageModels/VacanciesSearchPage.cs
@@ -0,0 +1,73 @@
+﻿
+using OpenQA.Selenium;
+using System.Collections.Generic;
+
+namespace AutomationTask2.Tests.PageModels
+{
+    public class VacanciesSearchPage
+    {
+        private IWebDriver _driver;
+
+        public VacanciesSearchPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public IWebElement CookieScriptClose
+        {
+            get
+            {
+                try
+                {
+                    return _driver.FindElement(By.Id("cookiescript_close"));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        public IWebElement Keywords => _driver.FindElement(By.XPath("//input[@placeholder='Keyword']"));
+
+        //ClearForm() must be called before this this function
+        public VacanciesSearchPage SelectDepartment(string text)
+        {
+            var departments = _driver.FindElement(By.XPath("//button[contains(text(),'All departments')]"));
+            departments.Click();
+            var link = _driver.FindElement(By.XPath($"//a[contains(text(),'{text}')]"));
+            link.Click();
+            return this;
+        }
+
+        //ClearForm() must be called before this this function
+        public VacanciesSearchPage SelectLanguages(string[] languages)
+        {
+            var langs = _driver.FindElement(By.XPath("//button[contains(text(),'All languages')]"));
+            langs.Click();
+            foreach (var lang in languages)
+            {
+                var label = _driver.FindElement(By.XPath($"//label[contains(text(),'{lang}')]"));
+                var checkbox = _driver.FindElement(By.Id(label.GetAttribute("for")));
+                checkbox.Click();
+            }
+            return this;
+        }
+
+        public void ClearForm()
+        {
+            //make sure 'clear filter' is clicked
+            var script = @"var btn = document.evaluate(""//button[text()='Clear filters']"", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; btn.click();";
+            ((IJavaScriptExecutor)_driver).ExecuteScript(script);
+        }
+
+        public VacanciesSearchPage Navigate(string v)
+        {
+            _driver.Navigate().GoToUrl(v);
+            return this;
+        }
+
+        public IReadOnlyList<IWebElement> SearchResults
+            => _driver.FindElements(By.CssSelector("a[data-vacancy]:not([data-vacancy=''])"));
+    }
+}
---
+++ AutomationTask2.Tests/VacanciesSearchResultTests.cs
@@ -0,0 +1,59 @@
+﻿using AutomationTask2.Tests.PageModels;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationTask2.Tests
+{
+    public class VacanciesSearchResultTests
+    {
+        protected VacanciesSearchPage vPage;
+        protected IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public void OpenPage()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            vPage = new VacanciesSearchPage(_driver);
+            vPage.Navigate("https://cz.careers.veeam.com/vacancies");
+            if (vPage.CookieScriptClose != null)
+            {
+                vPage.CookieScriptClose.Click();
+            }
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            _driver.Quit();
+        }
+
+        [SetUp]
+        public void ClearForm()
+        {
+            vPage.ClearForm();
+        }
+
+        [Test]
+        [TestCase("", "Research & Development", new[] { "English" }, 11)]
+        [TestCase("Webdriver C# best practices", "Research & Development", new[] { "English" }, 0)]
+        [TestCase("Page Object", "Research & Development", new[] { "English" }, 0)]
+        [TestCase("implicit/explicit waits", "Research & Development", new[] { "English" }, 0)]
+        [TestCase("reliable/fragile locators", "Research & Development", new[] { "English" }, 0)]
+        [TestCase("NUnit", "Research & Development", new[] { "English" }, 0)]
+        [TestCase("C#", "Research & Development", new[] { "English" }, 7)]
+        public void SearchResultsBeCorrect(string keyword, string department, string[] languages, int numberOfResults)
+        {
+            vPage
+                .SelectDepartment(department)
+                .SelectLanguages(languages)
+                .Keywords.Clear();
+            vPage
+                .Keywords.SendKeys(keyword);
+            Assert.That(vPage
+                .SearchResults
+                .Count, Is.EqualTo(numberOfResults));
+        }
+    }
+}
